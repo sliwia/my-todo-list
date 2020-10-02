f51f6e5fb02621b8a86b0187611449b584709fb4
Liwia <liwia@macbook-air-liwia.home>
2020-10-02 19:53:06 +0200

    Updates

---
+++ precache-manifest.0e9af721ad1f6e0a878fe170036ab83f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c21ccc4bded2f878097aa795f8ad3f",
+    "url": "/my-todo-list/index.html"
+  },
+  {
+    "revision": "2a6fea555509817db0eb",
+    "url": "/my-todo-list/static/css/main.c0d40dcb.chunk.css"
+  },
+  {
+    "revision": "30b576e97ccc327b1e5b",
+    "url": "/my-todo-list/static/js/2.786d709c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/my-todo-list/static/js/2.786d709c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a6fea555509817db0eb",
+    "url": "/my-todo-list/static/js/main.2148cd93.chunk.js"
+  },
+  {
+    "revision": "30e3b95480c8544da2ef",
+    "url": "/my-todo-list/static/js/runtime-main.18a084b2.js"
+  }
+]);
